e8f5d1ca848f5da131db95158f4de57715fe03ed
cnxiaohei <441900894@qq.com>
2022-11-02 20:50:39 +0800

    init

---
+++ src/main/java/com/xiaohei/Main.java
@@ -0,0 +1,12 @@
+package com.xiaohei;
+
+
+/**
+ * @Author: Xiaohei
+ * @CreateTime: ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
